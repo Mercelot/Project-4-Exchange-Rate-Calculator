af35f667b7d4903fd7dcead752acd1f2ec9bc772
Shane Audinett <shaneaudinett@gmail.com>
2022-11-03 09:39:38 +0000

    Added full JS functionality to exchange rate calculator

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-function calculate() {
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
 
+const rateEL = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+// Fetch exchange rates and update the dom.
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", "p9C3DJajRKNnw1hZHxffQK0rXOrfDkj3");
+
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+
+    fetch(`https://api.apilayer.com/exchangerates_data/latest?base=${currency_one}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            const rate = result.rates[currency_two];
+            rateEL.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        })
+        //.catch(error => console.log('error', error));
 }
+
+// Event Listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+})
 
 calculate();
 
